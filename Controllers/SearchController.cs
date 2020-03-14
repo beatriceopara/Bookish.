c1b9ebae57872a9ae2a9843ad23bcfb70ce121d3
Beatrice <beatrice.opara@softwire.com>
2020-03-14 14:55:19 +0000

    made search MVC created form in search.cshtml created table in searchresults.cshtml

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Bookish1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using MySql.Data.MySqlClient;
+// using MySql.Data.MySqlClient.Connection;
+
+
+namespace Bookish1.Controllers
+{
+    
+    public class SearchController:Controller
+    {
+        [Route("search")]
+        public IActionResult Search()
+        {
+            return View("Search");
+        }
+
+        [HttpPost("/results")]
+
+        public IActionResult SearchResults()
+        {
+            var connectionString = "Server=localhost;Database=library;Uid=root;Pwd=sugar;";
+            using var connection = new MySqlConnection(connectionString);
+
+            var searchResults = connection.Query<SearchModel>("SELECT * FROM books WHERE authorLastName = 'Rowling'").ToList();
+            
+            searchResults.ForEach(Console.Write);
+
+            return View(searchResults);
+        }
+        
+    }
+
+}
---
+++ Models/SearchModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Bookish1.Models
+{
+    public class SearchModel
+    {
+        public string Title { get; set; }
+        
+        public string AuthorFirstName {get; set; }
+        
+        public string AuthorLastName {get; set; }
+        
+        public string PublishedYear {get; set; }
+    }
+}
