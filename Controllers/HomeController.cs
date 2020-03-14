055f6dc3864eead3728b8643370ee9f65bc3816a
Beatrice <beatrice.opara@softwire.com>
2020-03-14 13:15:25 +0000

    first commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,20 +18,21 @@ namespace Bookish1.Controllers
             _logger = logger;
         }
 
+        [Route("")]
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
-
+        [Route("/privacy")]
         public IActionResult Privacy()
         {
-            return View();
+            return View("Privacy");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -18,9 +18,6 @@ namespace Bookish1
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
